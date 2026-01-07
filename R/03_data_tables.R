47d4f07ca4272ddb99f1656425d9c86227e8cd29
Sarah E. Wright <sarah_wright@nps.gov>
2026-01-07 23:12:56 +0000

    start refactoring data table module

--- R/03_data_tables.R
+++ R/03_data_tables.R
@@ -1,4 +1,4 @@
-tableMetadataUI <- function(id) {
+tableUploadUI <- function(id) {
   layout_columns(
     card(
       card_header("Upload data tables"),
@@ -18,10 +18,8 @@ tableMetadataUI <- function(id) {
   )
 }
 
-tableMetadataServer <- function(id) {
+tableUploadServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-    uploaded_files <- tibble::tibble()
-    
     table_metadata <- reactive({
       req(input$upload)
       
@@ -34,59 +32,8 @@ tableMetadataServer <- function(id) {
                       table_name = NA,
                       description = NA) %>%
         dplyr::select(file_name, table_name, description, size_mb, file_loc)
-      
-      if (nrow(uploaded_files) > 0 && any(new_files$file_name %in% uploaded_files$file_name)) {
-        dups <- new_files$file_name[new_files$file_name %in% uploaded_files$file_name]
-        
-        if (length(dups) == nrow(new_files)) {
-          new_files <- NULL
-        } else {
-          new_files <- dplyr::filter(new_files, !(file_name %in% dups))
-        }
-        
-        showModal(
-          modalDialog(
-            title = "WARNING: Duplicate Files",
-            easyClose = FALSE, 
-            p(glue::glue("You cannot upload multiple files with the same name. Ignoring the following duplicates: {toString(dups)}")),
-            footer = modalButton("Dismiss")
-          )
-        )
-      }
-      uploaded_files <<- rbind(uploaded_files, new_files)
     })
-    output$file_table <- DT::renderDT(
-      table_metadata(),
-      selection = "none",
-      server = FALSE,
-      rownames = FALSE,
-      editable = list(
-        target = "all",
-        disable = list(columns = 0)
-      ),
-      options = list(dom = 't',
-                     columnDefs = list(
-                       list(
-                         targets = c("file_loc", "size_mb"),
-                         visible = FALSE
-                       )
-                     )
-      ),
-      colnames = c("File name",
-                   "Name",
-                   "Description"
-      )
-    )
-    data <- reactive({
-      req(input$upload)
-      purrr::map(input$upload$datapath, readr::read_csv) %>%
-        purrr::set_names(input$upload$name)
-    })
-    
-    reactive({
-      DT::editData(uploaded_files, input$file_table_cell_edit, rownames = FALSE)
-      DT::editData(table_metadata(), input$file_table_cell_edit, rownames = FALSE)
-    })
+    return(table_metadata)
   })
 }
 
